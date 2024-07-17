119ca5a6b659301d6d915fdcb1ca594c54a9f286
Konstantin Plotkin <koostaac@gmail.com>
2024-07-17 16:03:21 +0300

    Homework

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,7 +1,6 @@
 import unittest
 
-from homework5.bank import (Account, AccountNotFound, Bank, Client,
-                            CurrencyMismatch, InsufficientFunds)
+from homework5.bank import (Account,  Bank)
 
 
 class TestClient(unittest.TestCase):
@@ -10,45 +9,44 @@ class TestClient(unittest.TestCase):
         self.bank = Bank()
         self.bank.add_client("1", "John")
         self.bank.add_client("2", "Cena")
+        self.client1 = self.bank.get_client("1")
+        self.client2 = self.bank.get_client("2")
 
     def test_open_account(self):
-        self.client.open_account("USD")
-        self.assertIn("USD", self.client.accounts)
         self.client1.open_account("USD")
         self.assertIn("USD", self.client1.accounts)
+        self.client2.open_account("EUR")
+        self.assertIn("EUR", self.client2.accounts)
 
     def test_close_account(self):
-        self.client.open_account("USD")
-        self.client.close_account("USD")
-        self.assertNotIn("USD", self.client.accounts)
+        self.client1.open_account("USD")
+        self.client1.close_account("USD")
+        self.assertNotIn("USD", self.client1.accounts)
 
     def test_get_account(self):
-        self.client.open_account("USD")
-        account = self.client.get_account("USD")
+        self.client1.open_account("USD")
+        account = self.client1.get_account("USD")
         self.assertIsInstance(account, Account)
 
     def test_deposit(self):
-        self.client.open_account("USD")
-        account = self.client.get_account("USD")
+        self.client1.open_account("USD")
+        account = self.client1.get_account("USD")
         account.deposit(100.0)
         self.assertEqual(account.balance, 100.0)
 
     def test_withdraw(self):
-        self.client.open_account("USD")
-        account = self.client.get_account("USD")
+        self.client1.open_account("USD")
+        account = self.client1.get_account("USD")
         account.deposit(100.0)
         account.withdraw(50.0)
         self.assertEqual(account.balance, 50.0)
 
     def test_transfer(self):
-        client1 = self.bank.get_client("1")
-        client2 = self.bank.get_client("2")
+        self.client1.open_account("USD")
+        self.client2.open_account("USD")
 
-        client1.open_account("USD")
-        client2.open_account("USD")
-
-        usd_account1 = client1.get_account("USD")
-        usd_account2 = client2.get_account("USD")
+        usd_account1 = self.client1.get_account("USD")
+        usd_account2 = self.client2.get_account("USD")
 
         usd_account1.deposit(100.0)
 
