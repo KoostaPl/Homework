3711ffb6943eb1db1e7e2d6abb201d874f0dc0fa
Konstantin Plotkin <koostaac@gmail.com>
2024-07-17 15:51:20 +0300

    Homework

--- homework1/task_01.py
+++
@@ -1,9 +0,0 @@
-# ДЗ из лекции 2
-val = int(input('Введите число: '))
-if val % 17:
-    if val % 2:
-        print('Число:', 'odd')
-    else:
-        print('Число:','even')
-else:
-    print('Число:','odd')
